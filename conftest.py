142a6b7546bc0d9a82388d78e9371c0c37e2bfbd
Rudyasha <55089144+Rudyasha@users.noreply.github.com>
2020-05-16 21:24:44 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,28 @@
+import pytest
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default=ru,
                      help="Choose language")
-@pytest.mark.parametrize('browser', ["Chrome", "Firefox"])
+      parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser chrome or firefox")
+        
+@pytest.fixsture(scope='function')
 def browser(request):
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.{browser}()
-
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    if browser_name == 'chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options = options)
+        browser.implicitly_wait(5)
+    elif browser_name == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.implicitly_wait(5)
+    else: 
+        raise pytest.UsageError("--Browser should be chrome or firefox")
+    browser.quite()
 
