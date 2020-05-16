a245b05cfae21c772ffd4607d21dad96991d9318
Rudyasha <55089144+Rudyasha@users.noreply.github.com>
2020-05-16 21:10:06 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
+from selenium import chromedriver
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
+def test_basket():
+  browser.get(link)
+  time.sleep(30)
+  basket = browser.find_element_by_xpath(//button[@class='btn btn-lg btn-primary btn-add-to-basket'])
+  
+  assert len(basket) > 0
